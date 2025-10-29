a51a07234fc4c6c4f6cb1a2700bf19ece07f7683
codingmaster094 <codingmaster094@gmail.com>
2025-10-29 16:07:20 +0530

    add new changes

--- src/components/SEO/config.ts
+++ src/components/SEO/config.ts
@@ -23,6 +23,44 @@ export const SEO: Field = {
         MetaDescriptionField({ hasGenerateFn: true }),
         MetaImageField({ relationTo: 'media' }),
         {
+          name: 'indexing',
+          type: 'radio',
+          label: 'Indexing',
+          options: [
+            {
+              label: 'Index',
+              value: 'index',
+            },
+            {
+              label: 'No Index',
+              value: 'noindex',
+            },
+          ],
+          defaultValue: 'index',
+          admin: {
+            layout: 'horizontal',
+          },
+        },
+        {
+          name: 'following',
+          type: 'radio',
+          label: 'Link Following',
+          options: [
+            {
+              label: 'Follow',
+              value: 'follow',
+            },
+            {
+              label: 'No Follow',
+              value: 'nofollow',
+            },
+          ],
+          defaultValue: 'follow',
+          admin: {
+            layout: 'horizontal',
+          },
+        },
+        {
           name: 'canonicalUrl',
           label: 'Canonical URL',
           type: 'text',
@@ -37,78 +75,6 @@ export const SEO: Field = {
           imagePath: 'meta.image',
         }),
       ],
-    },
-    {
-      name: 'structuredData',
-      type: 'json',
-      admin: {
-        hidden: true,
-      },
-      hooks: {
-        afterRead: [
-          ({ siblingData }) => {
-            return {
-              '@context': 'https://schema.org',
-              '@graph': [
-                {
-                  '@type': ['Organization', 'CommunityHealth'],
-                  '@id': 'https://protrance.de/#organization',
-                  name: 'Protrance',
-                  url: 'https://rauchfreidurchhypnose.de/',
-                  email: 'info@protrance.de',
-                  logo: {
-                    '@type': 'ImageObject',
-                    '@id': 'https://protrance.de/#logo',
-                    url: 'https://rauchfreidurchhypnose.de/wp-content/uploads/2021/07/1024x1024_fav_1024x1024_logomark-1.png',
-                    contentUrl:
-                      'https://rauchfreidurchhypnose.de/wp-content/uploads/2021/07/1024x1024_fav_1024x1024_logomark-1.png',
-                    caption: 'Protrance',
-                    inLanguage: 'de',
-                    width: 1024,
-                    height: 1024,
-                  },
-                  address: {
-                    '@type': 'PostalAddress',
-                    streetAddress: 'Alte Poststr. 12',
-                    addressLocality: 'Uhldingen',
-                    addressRegion: 'Baden-Württemberg',
-                    postalCode: '88690',
-                    addressCountry: {
-                      '@type': 'Country',
-                      name: 'Deutschland',
-                    },
-                  },
-                  contactPoint: {
-                    '@type': 'ContactPoint',
-                    telephone: '+49 7556 2476 007',
-                    contactType: 'customer support',
-                  },
-                  openingHours: [
-                    'Monday,Tuesday,Wednesday 09:00-18:00',
-                    'Thursday 09:00-12:00',
-                    'Friday 14:00-18:00',
-                  ],
-                  location: {
-                    '@type': 'Place',
-                    '@id': 'https://protrance.de/#place',
-                    address: {
-                      '@type': 'PostalAddress',
-                      streetAddress: 'Alte Poststr. 12',
-                      addressLocality: 'Uhldingen',
-                      addressRegion: 'Baden-Württemberg',
-                      postalCode: '88690',
-                      addressCountry: {
-                        '@type': 'Country',
-                        name: 'Deutschland',
-                      },
-                    },
-                  },
-                },
-              ],
-            }
-          },
-        ],
-      },
     },
   ],
 }
