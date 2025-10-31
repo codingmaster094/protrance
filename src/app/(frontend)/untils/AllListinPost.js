0f02e90736376177a1e20c0bf0392e62995b4401
codingmaster094 <codingmaster094@gmail.com>
2025-10-31 19:05:25 +0530

    solved error

--- src/app/(frontend)/untils/AllListinPost.js
+++ src/app/(frontend)/untils/AllListinPost.js
@@ -1,21 +1,10 @@
 export default async function AllListinPost(params) {
-  try {
-    const response = await fetch(
-      `${
-        process.env.NEXT_POST_LISTING_BASE_URL ||
-        "https://protrance.vercel.app/api"
-      }${params}`,
-      { next: { revalidate: 0 } }
-    );
-    if (!response) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
-    }
-
-    const data = await response.json();
-
-    return data;
-  } catch (error) {
-    console.error("Error in Alldata:", error);
-    throw error; // Rethrow the error to be caught in the calling component
+  const response = await fetch(
+    `${process.env.NEXT_POST_LISTING_BASE_URL || "https://protrance.vercel.app/api"}${params}`,
+    { next: { revalidate: 3600 } } // must be positive, NOT zero
+  );
+  if (!response.ok) {
+    throw new Error(`Failed to fetch data: ${response.statusText}`);
   }
+  return await response.json();
 }
