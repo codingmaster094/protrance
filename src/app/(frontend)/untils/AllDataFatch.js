06fb1dbd942b41b0b41430dd712dee2b9b239eda
codingmaster094 <codingmaster094@gmail.com>
2025-10-31 19:13:47 +0530

    solved error

--- src/app/(frontend)/untils/AllDataFatch.js
+++ src/app/(frontend)/untils/AllDataFatch.js
@@ -4,9 +4,12 @@ export default async function Alldata(params) {
       `${process.env.NEXT_PUBLIC_BASE_URL || "https://protrance.vercel.app/api/globals"}${params}`,
       { next: { revalidate: 3600 } }
     );
+
     if (!response.ok) {
-      throw new Error(`Failed to fetch data: ${response.statusText}`);
+      const errorText = await response.text();
+      throw new Error(`Failed to fetch data: ${response.status} ${errorText}`);
     }
+
     const data = await response.json();
     return data;
   } catch (error) {
